ea74558ff07ca61b29ae8f653a78099f2cef611e
tarasevich-dmitry <tarasevichdmr@gmail.com>
2020-11-16 15:42:12 +0200

    1 release update

--- app/dota4cast.py
+++ app/dota4cast.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# This demo lets you to explore dot4cast ...
+# This demo lets you to explore dot4cast project, Dota 2 Esports analytics web app
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
@@ -27,19 +27,24 @@ from math import sqrt, log, log2
 from trueskill import BETA
 from trueskill.backends import cdf
 from joblib import dump, load
+import pydeck as pdk
 import io
 
 def main():
     #Here used unsafe function to set canonical url for google search, MAY BE REMOVED FROM STREAMLIT!!!
     st.write('<link rel="canonical" href="http://www.dota4cast.ml/" />',unsafe_allow_html=True)
 
-
     st.title('Dota4cast Beta')
 
     st.write("""
-    **Dota4cast.ml is a machine learning based platform that is building Dota 2 largest library of analytical tools, odds predicting tools and data to make that information accessible and useful for all Dota players and researchers.**
+    **It is absolutely free and opensource project, check out source code [Github](https://github.com/tarasevich-dmitry/Dota4cast)**
     """)
 
+    st.write("""
+    **Dota4cast.ml is a machine learning based platform that is building Dota 2 analytical tools, odds prediction and gathering data to make that information accessible and useful for all Dota players and researchers.**
+    """)
+
+    #main front image
     image = Image.open('main_img.jpg')
     st.image(image, caption=' ', use_column_width=True)
 
@@ -54,15 +59,14 @@ def main():
     Our solution will bring your betting experience on a new level.
     """
 
-
     #Put mmr widgets in a sidebar
-    df_mmr = pd.DataFrame({
-      'first column': [1, 2, 3, 4, 5, 6, 7, 8]
-    })
+    #df_mmr = pd.DataFrame({
+    #  'first column': [1, 2, 3, 4, 5, 6, 7, 8]
+    #})
 
-    option = st.sidebar.selectbox(
-        'Chouse option what to do',
-         df_mmr['first column'])
+    #option = st.sidebar.selectbox(
+    #    'Chouse option what to do',
+    #     df_mmr['first column'])
 
 
     #appears on bottom:
@@ -74,12 +78,6 @@ def main():
     #Upload data
     df2 = pd.read_csv("selected_team_matches.csv")
     matches_info = pd.read_csv('selected_team_matches.csv').sort_values(['match_id'])
-
-    #Get all team names from df
-    #"""
-    #Check all available teams for analisys:
-    #"""
-    #st.write(matches_info.radiant.unique())
 
     trueskill.setup(draw_probability=0)
 
@@ -109,11 +107,9 @@ def main():
 
 
     if st.button('Can`t find your team?'):
-        #st.image(image_s, caption=' ', use_column_width=True)
-        st.write('Contact us on e-mail: dota4cast@gmail.com')
+        st.write('Unfortunately some data can be outdated, we working on making data pipeline more reliable. Have suggestions? - Contact us on e-mail: dota4cast@gmail.com')
 
-
-    #Put mdropdown menu wuth teams
+    #Put dropdown menu with teams
     df_teams = matches_info.radiant.unique()
 
 
@@ -129,72 +125,112 @@ def main():
     st.write(option1,'**Win probability is:**', win_probability(Rates[option1], Rates[option2]))
     #####################################################################################
 
-
-
-    ###################################################################################
-    #map
+    #map  TBD
     """
-
     **Tier 1 Tournaments map**
     """
 
-    map_data=pd.read_csv('map_coords.csv')
-
+    image2 = Image.open('map_tir1.png')
+    st.image(image2, caption=' ', use_column_width=True)
+    #map_data=pd.read_csv('map_coords.csv')
 
     # Adding code so we can have map default to the center of the data
-    midpoint = (np.average(map_data['latitude']), np.average(map_data['longitude']))
+    #midpoint = (np.average(map_data['latitude']), np.average(map_data['longitude']))
 
-    st.deck_gl_chart(
-                viewport={
-                    'latitude': midpoint[0],
-                    'longitude':  midpoint[1],
-                    'zoom': 1
-                },
-                layers=[{
-                    'type': 'ScatterplotLayer',
-                    'data': map_data,
-                    'radiusScale': 250,
-       'radiusMinPixels': 4,
-                    'getFillColor': [248, 24, 148],
-                }]
-            )
+
+    #st.pydeck_chart(pdk.Deck(
+    #    map_style='mapbox://styles/mapbox/light-v9',
+    #    initial_view_state=pdk.ViewState(
+    #        latitude=midpoint[0],
+    #        longitude=midpoint[1],
+    #        zoom=1,
+    #        pitch=50,
+    #    ),
+    #    layers = pdk.Layer(
+    #           'ScatterplotLayer',
+    #           data=map_data,
+    #           get_position='[lon, lat]',
+    #           auto_highlight=True,
+    #           radius=200,
+    #           elevation_scale=4,
+    #           elevation_range=[0, 1000],
+    #           pickable=True,
+    #           extruded=True,
+    #        ),
+    #))
+
+
     """
     Tier 1 Tournaments is typically tournaments with an outstanding prize pool, as a rule played offline, and feature the best world teams. They are commonly held by well-established franchises or Valve, considered especially prestigious amongst the community.
     """
-    ###################################################################################
-
 
 
     ###################################################################################
-    #Based on picks prediction
+    #Wordcloud
+    """
+    **Dota 2 All Chat Wordcloud**
+    """
+
+    image_wc = Image.open('wc_dota.png')
+    st.image(image_wc, caption=' ', use_column_width=True)
+
+    """
+    Dota 2 is notorious for having a somewhat toxic community. We construct a word cloud to inspect these claims and determine the frequency and flavour of the most common words. We get data from in-game all-chat.
+    """
+
+    ###################################################################################
+    #Based on picks prediction TBD
+    ###
+#    """
+#    **Pick win prediction:**
+#    """
+
     clf = load('f_model.joblib')
+    #load hero list:
+    heroes=pd.read_csv('hero_names.txt')
+
+    df_heroes = heroes.Sven.unique()
 
 
+    #col1, col2 = st.beta_columns(2)
 
-    
+    #ancient_1 = st.selectbox('1 radiant hero', df_heroes)
+    #ancient_2 = st.selectbox('2 radiant hero', df_heroes)
+    #ancient_3 = st.selectbox('3 radiant hero', df_heroes)
+    #ancient_4 = st.selectbox('4 radiant hero', df_heroes)
+    #ancient_5 = st.selectbox('5 radiant hero', df_heroes)
+
+    #dire_1 = st.selectbox('1 dire hero', df_heroes)
+    #dire_2 = st.selectbox('2 dire hero', df_heroes)
+    #dire_3 = st.selectbox('3 dire hero', df_heroes)
+    #dire_4 = st.selectbox('4 dire hero', df_heroes)
+    #dire_5 = st.selectbox('5 dire hero', df_heroes)
+
+    #st.write(option3,'**TEST:**')
+
     ###################################################################################
+    if st.button('Source code (◕‿◕)♡'):
+        st.write('https://github.com/tarasevich-dmitry/Dota4cast')
+
     if st.button('Contact us ٩(◕‿◕｡)۶'):
-        #st.image(image_suck, caption=' ', use_column_width=True)
         st.write('dota4cast@gmail.com')
 
     url_twitter = 'https://twitter.com/dota4cast'
 
     if st.button('(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ follow twitter'):
-        webbrowser.open_new_tab(url_twitter)
+        st.write('https://twitter.com/dota4cast')
+        #webbrowser.open_new_tab(url_twitter)
 
     """
                 *Copyright © 2020 dota4cast.ml All Rights Reserved. Dota 2 is a registered trademark of Valve Corporation. All game images and names are property of Valve Corporation.*
     """
 
-
-    #xD
+    #rofl ballons animation
     st.balloons()
 
 if __name__ == "__main__":
     main()
 
-#df = pd.read_csv("data.csv")
-#st.line_chart(df)
 
-#Atl+shift+t to open terminal
-#streamlit run main.py
+#Atl+shift+t Atom sc
+#To run app: streamlit run dota4cast.py
